4aceede7c1fdb170149523c66fd4df7c6c7042b7
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-05-08 18:54:02 -0500

    build node function

--- bst/bst.py
+++ bst/bst.py
@@ -5,7 +5,20 @@ class Node:
         self.left = None
         self.right = None
 
+
 # Tree class
 class Tree:
     def __init__(self, values):
         self.root = self.build_tree(values)
+
+    def build_tree(self, values):
+        # Use recursion to build the tree
+        def build_node(arr, start, end):
+            if start > end:
+                return None
+
+            mid = (start + end) // 2
+            node = Node(arr[mid])
+            node.left = build_node(arr, start, mid - 1)
+            node.right = build_node(arr, mid + 1, end)
+            return node
