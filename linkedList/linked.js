4ca394a1e600ee7e997d8eac33c4eac19df03f11
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-05-08 14:33:19 -0500

    add node to end of list

--- linkedList/linked.js
+++ linkedList/linked.js
@@ -13,4 +13,17 @@ class LinkedList {
     this.tail = null;
     this.size = 0;
   }
+
+  // Add node to the end of the list
+  append(value) {
+    const newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.nextNode = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
 }
