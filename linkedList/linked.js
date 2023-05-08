172c2168679bd2667c69ec59adcc8f540e575a88
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-05-08 14:31:32 -0500

    linked list class and constructor function

--- linkedList/linked.js
+++ linkedList/linked.js
@@ -5,3 +5,12 @@ class Node {
     this.nextNode = null;
   }
 }
+
+// Linked List class
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+}
