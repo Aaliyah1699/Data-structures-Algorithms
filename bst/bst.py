70d8f68d96ed9bbc2dd81737c8c306bb0107531b
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-05-08 19:04:54 -0500

    refactoring build tree function

--- bst/bst.py
+++ bst/bst.py
@@ -11,20 +11,6 @@ class Tree:
     def __init__(self, values):
         self.root = self.build_tree(values)
 
-    def build_tree(self, values):
-        # Use recursion to build the tree
-        def build_node(arr, start, end):
-            if start > end:
-                return None
-
-            mid = (start + end) // 2
-            node = Node(arr[mid])
-            node.left = build_node(arr, start, mid - 1)
-            node.right = build_node(arr, mid + 1, end)
-            return node
+    def build_tree(self, array):
+    
         
-        # Sort the array
-        values.sort()
-
-        # Call the recursive function
-        return build_node(values, 0, len(values) - 1)
