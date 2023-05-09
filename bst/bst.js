eb148e067b72c8a85a6c32b6a8318c27fc25b59a
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-05-08 20:59:14 -0500

    build tree func split array

--- bst/bst.js
+++ bst/bst.js
@@ -12,4 +12,22 @@ class Tree {
   constructor(array) {
     this.root = this.buildTree([...new Set(array)].sort());
   }
+
+  // Build tree
+  buildTree(array) {
+    // Base case
+    if (!array.length) {
+      return null;
+    }
+
+    // find middle and make it root
+    const mid = Math.floor(array.length / 2);
+    const root = new Node(array[mid]);
+
+    // Recursively build left and right subtree
+    root.left = this.buildTree(array.slice(0, mid));
+    root.right = this.buildTree(array.slice(mid + 1));
+
+    return root;
+  }
 }
